14ba666fa37bfdf21c0312eeb4ab0ca4613cb9ea
Lucas Cantarino <lucas.cantarino@engenharia.ufjf.br>
2020-04-21 17:13:28 -0300

    Foram adicionados aos códigos FOB.m e FOB_versao_de_testes.m cálculos do tempo de subida, máximo sobressinal e tempo de acomodação.  Porém, foi identificado que a resposta ao degrau no tempo não está se comportando como previsto.

--- FOB.m
+++ FOB.m
@@ -8,6 +8,29 @@ RespostaDegrau = (k(1) + k(2)/s + (k(3)*s)/(k(4)*s + 1))/(s*((k(1) + k(2)/s + (k
 syms t;
 funcaoNoTempo = vpa(ilaplace(RespostaDegrau,t))
 
+% A seguir, serão calculados alguns parâmetros que podem servir de base
+% para avaliação da resposta ao degrau
+
+flagTs = 0; flagMp = 0; flagSt = 0;
+funcaoNoTempoNumAnterior = 0;
+for i=0.0001:0.000001:0.001
+    funcaoNoTempoNum = subs(funcaoNoTempo,t,i)
+    if (flagTs == 0 && funcaoNoTempoNum>=1)   % Tempo de subida
+        Ts = i
+        flagTs = 1;
+    end
+    if (flagMp == 0 && funcaoNoTempoNum<funcaoNoTempoNumAnterior) % Máximo sobressinal
+        Mp = funcaoNoTempoNumAnterior
+        flagMp = 1;
+        flagSt = 1;
+    end
+    if (flagSt == 1 && funcaoNoTempoNum<=1.02) % Tempo de acomodação
+        St = i
+        break
+    end
+    funcaoNoTempoNumAnterior = funcaoNoTempoNum;
+end
+
 % O erro é calculado como diferença entre o degrau de referência e a
 % integrau da função da resposta ao degrau. O erro é calculado do tempo t = 0 até o tempo t = 0.1 
 
--- FOB_versao_de_testes.m
+++ FOB_versao_de_testes.m
@@ -14,18 +14,40 @@ clc; clear all;
 % C = Kp + Ki/s + Kd*s/(Tf*s+1)
 % F = P*C/(1+P*C)
 % RespostaDegrau = F/s
-% Kp = 2; Ki = 3; Kd = 4; Tf = 5;
+% Kp = 261.3; Ki = 6015; Kd = 2691; Tf = 0.0004506;
 % RespostaDegrauNum = subs(RespostaDegrau)
 
 syms Kp; syms Ki; syms Kd; syms Tf; syms s;
 RespostaDegrau = (Kp + Ki/s + (Kd*s)/(Tf*s + 1))/(s*((Kp + Ki/s + (Kd*s)/(Tf*s + 1))/((s/8 + 1)*((11*s)/200 + 1)) + 1)*(s/8 + 1)*((11*s)/200 + 1))
-Kp = 2; Ki = 3; Kd = 4; Tf = 5;
+Kp = 261.3; Ki = 6015; Kd = 2691; Tf = 0.0004506;
 RespostaDegrauNum = subs(RespostaDegrau)
 % Calculando a inversa de laplace
 
 syms t;
 funcaoNoTempo = vpa(ilaplace(RespostaDegrauNum,t))
 
+% A seguir, serão calculados alguns parâmetros que podem servir de base
+% para avaliação da resposta ao degrau
+
+flagTs = 0; flagMp = 0; flagSt = 0;
+funcaoNoTempoNumAnterior = 0;
+for i=0.0001:0.000001:0.001
+    funcaoNoTempoNum = subs(funcaoNoTempo,t,i)
+    if (flagTs == 0 && funcaoNoTempoNum>=1)   % Tempo de subida
+        Ts = i
+        flagTs = 1;
+    end
+    if (flagMp == 0 && funcaoNoTempoNum<funcaoNoTempoNumAnterior) % Máximo sobressinal
+        Mp = funcaoNoTempoNumAnterior
+        flagMp = 1;
+        flagSt = 1;
+    end
+    if (flagSt == 1 && funcaoNoTempoNum<=1.02) % Tempo de acomodação
+        St = i
+        break
+    end
+    funcaoNoTempoNumAnterior = funcaoNoTempoNum;
+end
 % O erro é calculado como diferença entre o degrau de referência e a
 % integrau da função da resposta ao degrau. O erro é calculado do tempo t = 0 até o tempo t = 0.1 
 
