ec075551bfd5b8a354680ed1c3f4bf425d2249cd
Lucas Cantarino <lucas.cantarino@engenharia.ufjf.br>
2020-04-25 23:00:54 -0300

    Adicionadas penalidades à FOB nos arquivos FOB.m e FOB_versao_de_testes.m.

--- FOB.m
+++ FOB.m
@@ -32,8 +32,24 @@ for i=0.0001:0.0001:0.04
 end
 
 % O erro é calculado como diferença entre o degrau de referência e a
-% integrau da função da resposta ao degrau. O erro é calculado do tempo t = 0 até o tempo t = 0.1 
+% integrau da função da resposta ao degrau. O erro é calculado do tempo t = 0 até o tempo t = 0.04 
 
-erro = double(0.1 - int(funcaoNoTempo,t,0,0.1))
+erro = double(0.04 - int(funcaoNoTempo,t,0,0.04))*(1/Ts^2+1/(0.156*St)^2+(179*Mp)^2)/120000
 
-
+% A funçao de erro acima foi definida levando em consideração tempo de
+% subida (Ts), máximo sobressinal (Mp) e tempo de acomodação (St). 
+% O erro é calculado como diferença entre o degrau de referência e a
+% integrau da função da resposta ao degrau d tempo t = 0 até o tempo t = 0.04. 
+% O fator multiplicador de penalidade é um multiplicador relacionado a Ts, Mp e St.
+% Para parâmetros menores que 1(Ts e St), colocamos a multiplicador no denominador,
+% para que, caso seja elevado, aumentem o erro. Já o Mp colocamos no
+% multiplicador por ser positivo. Os parâmetros foram elevados ao quadrado
+% para intensificar o processo de eliminação dos indivíduos com parâmetros
+% muito diferentes do ideal. Além disso, as consantes que multiplicam os
+% parâmetros servem para balancear igualmente os impactos dos 3 parâmetros
+% na FOB. Mas para isso, é necessária uma referência. A referência
+% escolhida foi baseada nos parâmetros Kp, Ki, Kd e Tf fornecidos pelo PID
+% Tuner para a planta contínua P do código "Controle". As constantes foram
+% escolhidas de modo que para esses Kp, Ki, Kd e Tf dados, os fatores
+% 1/Ts^2, 1/(0.156*St)^2 e (179*Mp)^2 sejam cada um aproximadamente 40000. No
+% fim, o erro é dividido novamente por 120000 para compensar 3*40000.
