6717bb4ddfff150c89aec57e5da2d83ffb2cb647
Lucas Cantarino <lucas.cantarino@engenharia.ufjf.br>
2020-09-14 22:12:12 -0300

    Alterações no cálculo do observador de malha internas (ainda não funcional)

--- Observador_Malha_Interna.m
+++ Observador_Malha_Interna.m
@@ -40,13 +40,23 @@ PID_R = KpR + KiR*tf(1,[1 0]);
 MalhaInternaL = feedback(PID_L*G_L,1)
 MalhaInternaR = feedback(PID_R*G_R,1)
 
-MalhaInterna = [0.2 1 2.591 7.937];
-MalhaInterna = [0.2 1 2.591 7.937]/MalhaInterna(1)
-mi = roots(MalhaInterna)
+% Ambas malhas internas são iguais após o controle de malha fechada
+% A variável abaixo foi definida copiando os valores das malhas internas calculadas pelo próprio MATLAB 
+DenominadorMalhaInterna = [0.2 2.591 7.937];
+mi = roots(DenominadorMalhaInterna)
+% Os polos do observador devem ser de 2 a 5 vezes o do controlador segundo
+% os slides. Foi utilizado o sistema controlado realimentado pois o polo do
+% controlador sozinho é 0.
 miObservador = real(3.5*mi)
-alfa1 = MalhaInterna(2)
-syms s;
-(s-miObservador(1))*(s-miObservador(2))*(s-miObservador(3))
-Ke_L = Q_L*(alfa1 - a1_L)
-Ke_R = Q_R*(alfa1 - a1_R)
 
+%A variável abaixo representa os coeficientes do polinômio (s-miObservador(1))*(s-miObservador(2))
+DenominadorObservador = [1 12.955 4.9699]
+alfa1 = DenominadorObservador(2)
+alfa2 = DenominadorObservador(3)
+
+% A multiplicação não poderia dar certo pois Q_L e Q_R são 1x1, mas o
+% MATLAB está considerando como escalar e fazendo multiplicação escalar.
+Ke_L = Q_L*[alfa1 - a1_L;
+            alfa1 -   0 ]
+Ke_R = Q_R*[alfa1 - a1_R;
+            alfa1 -   0 ]
